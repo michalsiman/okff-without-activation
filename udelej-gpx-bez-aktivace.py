8db12a4c5bfc5859e27cbdca4484855ad5fe870d
Michal Šiman <michal.siman@gmail.com>
2022-12-12 11:42:07 +0100

    Update udelej-gpx-bez-aktivace.py

--- udelej-gpx-bez-aktivace.py
+++ udelej-gpx-bez-aktivace.py
@@ -1,6 +1,14 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # tento script vezme soubor *.csv ktery najde ve slozce, pak si vezme soubor okff.gpx ktery najde ve slozce
 # a vytvori soubor okff-noactivation.gpx ktery ulozi a v kterem budou pouze a jen okff ze souboru .csv doplnene o presnou polohu
 # cele to slouzi k tomu, aby se dala do LOCUSU pridat dalsi vrstva s OKFF ktere jeste nikdo neaktivoval
+
+# skript je funkcni v Linuxu Mint a Python3
+# pomoci "pip install" je treba naistalovat wget, zipfile
+
+# pro spravny beh pod windows10 je treba na radku 45 odstranit parametr "encoding" kompletne
 
 # udelal Michal OK1SIM pro komunitu OKFF ... 73 & 44
 # pouzivej dle libosti, upravuj, sir, proste je to tvoje ...
@@ -34,7 +42,7 @@ with open("okff-without-activation.gpx","a") as file:
     file.write('<?xml version="1.0" encoding="windows-1250" standalone="no" ?>\n\n')
     file.write('<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1" creator="GPS Data Team ( http://www.gps-data-team.com )">\n\n')
 
-with open('./okff/okff.gpx', encoding = 'ISO-8859-1') as f:
+with open('./okff/okff.gpx', encoding = 'ISO-8859-1') as f: # ve windows je třeba odstranit parametr encoding kompletně
     exist_lines = list(f)
     for exist_line in exist_lines:
         with open('bez-aktivace.csv') as b:
